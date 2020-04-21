0cfaddf3a28dd92faeb8ba19fd0bf27cf7f8c272
baskrasen <60425203+baskrasen@users.noreply.github.com>
2020-04-21 17:12:32 +0700

    Update BmiService.java

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
-    public double calculate(double BodyMass, double height) {
-        double BodyMassIndex = BodyMass / (height * height);
-        return BodyMassIndex;
+    public double calculate(double bodyMass, double height) {
+        double bodyMassIndex = BodyMass / (height * height);
+        return bodyMassIndex;
     }
 }
